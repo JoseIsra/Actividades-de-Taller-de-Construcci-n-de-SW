38e7c74870eed8a2e05f083806c44a82af107224
alexander <alexander.zavaleta@unmsm.edu.pe>
2020-11-25 21:04:00 -0500

    lorem

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <h1>HAPPY PET</h1>
     <h2>HAPPY PET</h2>
     <h3>SAD PET</h3>
+    <p>asdfasdfasdfasdfasdfasdfasdfasdf</p>
+    <h4>SAD PET</h4>
+    <p>lorem asdfasdfasdfasdfasdfasdfasdfasdfaasdfasdfasdfsdfasdfasdf</p>
     </div>
   );
 }
