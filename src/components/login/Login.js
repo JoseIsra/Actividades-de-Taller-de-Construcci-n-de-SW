d66468a39cf79466c75c378c116f2ca11e0de125
alexander <alexander.zavaleta@unmsm.edu.pe>
2020-11-28 14:35:39 -0500

    login2.0

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,8 +3,22 @@ import "./Login.css";
 
 export const Login = () => {
   return (
+    <div className="_login">
     <div className="Login">
-      <h2>Log in</h2>
+
+      <h2>Login</h2>
+
+      <form>
+        <input type="text" placeholder="Username"></input>
+        <br />
+        <br />
+        <input type="password" placeholder="Password"></input>
+        <br />
+        <button type="submit" className="enviar">
+            Login
+          </button> 
+      </form>
+    </div>
     </div>
   );
 }
