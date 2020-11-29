2665c228713db601984d82b3cddeaa477edd3ea3
Marco <marco.fura@unmsm.edu.pe>
2020-11-29 15:48:45 -0500

    about us casi terminado

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -9,10 +9,36 @@ export const Home=()=>{
 <Header/>
 <main>
     <section id="inicio" className="section">
-        <h2>Inicio</h2>
+        <div className="Inicio">
+            <div className="texto">
+                <h2>Comienza a explorar todos nuestros productos y servicios</h2>
+                <button type="button" className="btn btn-outline-info">Comenzar</button>
+            </div>
+        </div>
     </section>
     <section id="aboutUs" className="section">
-        <h2>Acerca de nosotros</h2>
+        <div className="AboutUs">
+            <h2>Acerca de nosotros</h2>
+            <div className="AboutUs2">
+                <div className="mision">
+                    <h4>Nuestra misión</h4>
+                    <p> Aqui irá todo con respecto a la mision Aqui irá todo con respecto a la mision Aqui irá todo con respecto a la mision Aqui irá todo con respecto a la mision </p>
+                </div>
+                <div className="vision">
+                    <h4>Nuestra visión</h4>
+                    <p>Aqui irá todo con respecto a la vision</p>
+                </div>
+                <div className="metas">
+                    <h4>Nuestras metas</h4>
+                    <p>Aqui irá todo con respecto a las metas</p>
+                </div>
+                <div className="proveedores">
+                    <h4>Nuestros proveedores</h4>
+                    <p>Aqui irá todo con respecto a nuestros proveedores</p>
+                </div>
+            </div>
+        </div>
+        
     </section>
     <section id="contactUs" className="section">
         <h2>Contáctanos</h2>
