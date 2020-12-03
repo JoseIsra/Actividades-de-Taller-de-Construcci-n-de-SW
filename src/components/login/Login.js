63e0c4263abd55ab7a5117c507c32e012b10aecc
alexander <alexander.zavaleta@unmsm.edu.pe>
2020-12-03 01:52:27 -0500

    linkto

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "./Login.css";
+import {Link} from 'react-router-dom'
 
 export const Login = () => {
   return (
     <div className="_login">
+      <header className="register__header">
+                <p>Happy Pet</p>
+            </header>
     <div className="Login">
 
       <h2>Login</h2>
@@ -11,12 +15,17 @@ export const Login = () => {
       <form>
         <input type="text" placeholder="Username"></input>
         <br />
-        <br />
+        
         <input type="password" placeholder="Password"></input>
         <br />
+        <div className="linkreg">
+          <Link to="/Register">no tienes cuenta?</Link>
+        </div>
+        
         <button type="submit" className="enviar">
             Login
           </button> 
+          
       </form>
     </div>
     </div>
