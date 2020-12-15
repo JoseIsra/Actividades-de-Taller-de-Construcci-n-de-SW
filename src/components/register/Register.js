5386a9ebd0883b1a9ce9c6d584ffc71b739d7b87
israel diaz <israelmyname@gmail.com>
2020-12-15 13:40:54 -0500

    registro bello

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -4,54 +4,66 @@ import './Register.css';
 import logoHappy from "../../images/logo-happypet.png";
 import { Link } from 'react-router-dom';
 export const Register = () => {
-    // const [user, setUser] = useState({
-    //     name: '',
-    //     lastname: '',
-    //     email: '',
-    //     password: ''
-    // })
+    const [user, setUser] = useState({
+        name: '',
+        lastname: '',
+        email: '',
+        password: ''
+    })
 
-    // const handleChange = (e) => {
-    //     setUser({
-    //         ...user,
-    //         [e.target.name]: e.target.value
-    //     });
-    // };
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+    };
 
 
     return (
         <div className="register">
-            
-                <Link to= "/home">
+
+            <Link to="/home">
                 <img className="hapyLogo" src={logoHappy} alt="logohappy" />
-                </Link>
-            
+            </Link>
+
             <div className="container">
-            <div className="registerForm">
-            <h2>Regístrate</h2>
-            <div className="registerBox">
-                <div className="inputBox w50">
-                    <input required type="text" />
-                    <span>NOMBRES</span>
-                </div>
-                <div className="inputBox w50">
-                    <input required type="text" />
-                    <span>APELLIDOS</span>
-                </div>
-                <div className="inputBox w50">
-                    <input required type="text" />
-                    <span>CORREO</span>
-                </div>
-                <div className="inputBox w50">
-                    <input required type="text" />
-                    <span>CONTRASEÑA</span>
-                </div>
-                <div className="inputBox w100">
-                    <input required type="submit" value="Guardar"/>
+                <div className="registerForm">
+                    <h2>Regístrate</h2>
+                    <div className="registerBox">
+                        <div className="inputBox w50">
+                            <input autoComplete="off" required type="text"
+                                name="name"
+                                onChange={handleChange}
+                            />
+                            <span>NOMBRES</span>
+                        </div>
+                        <div className="inputBox w50">
+                            <input autoComplete="off" required type="text"
+                                name="lastname"
+                                onChange={handleChange}
+                            />
+                            <span>APELLIDOS</span>
+                        </div>
+                        <div className="inputBox w50">
+                            <input autoComplete="off" required type="text"
+                                onChange={handleChange}
+                                name="email"
+                            />
+                            <span>CORREO</span>
+                        </div>
+                        <div className="inputBox w50">
+                            <input autoComplete="off" required type="pasword"
+                            onChange={handleChange}
+                            name="password"
+                            />
+                            <span>CONTRASEÑA</span>
+                        </div>
+                        <div className="inputBox w100">
+                            <input  type="submit" value="Guardar" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
 
         </div >
     )
