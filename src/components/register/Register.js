c7251d9bab0580958eb8d7f7d764d54c58c132ac
israel diaz <israelmyname@gmail.com>
2020-12-15 13:33:19 -0500

    form con nueva vista momentanea

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -4,63 +4,52 @@ import './Register.css';
 import logoHappy from "../../images/logo-happypet.png";
 import { Link } from 'react-router-dom';
 export const Register = () => {
-    const [user, setUser] = useState({
-        name: '',
-        lastname: '',
-        email: '',
-        password: ''
-    })
+    // const [user, setUser] = useState({
+    //     name: '',
+    //     lastname: '',
+    //     email: '',
+    //     password: ''
+    // })
 
-    const handleChange = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        });
-    };
+    // const handleChange = (e) => {
+    //     setUser({
+    //         ...user,
+    //         [e.target.name]: e.target.value
+    //     });
+    // };
 
 
     return (
         <div className="register">
-            <header className="register__header">
+            
                 <Link to= "/home">
-                <img src={logoHappy} alt="logohappy" />
+                <img className="hapyLogo" src={logoHappy} alt="logohappy" />
                 </Link>
-            </header>
-            <div className="register__card">
-                
+            
+            <div className="container">
+            <div className="registerForm">
             <h2>Regístrate</h2>
-            <div className="register__content">
-                <form autoComplete="off">
-                    <input autoFocus="on"
-                    type="text"
-                        name="name"
-                        value={user.name}
-                        placeholder="Nombre..."
-                        onChange={handleChange}
-                    />
-                    <input type="text"
-                        name="lastname"
-                        placeholder="Apellido..."
-                        value={user.lastname}
-                        onChange={handleChange}
-                    />
-                    <input type="text"
-                        name="email"
-                        placeholder="Correo..."
-                        value={user.email}
-                        onChange={handleChange}
-                    />
-                    <input type="password"
-                        value={user.password}
-                        name="password"
-                        placeholder="Contraseña..."
-                        onChange={handleChange}
-                    />
-
-                    <button type="submit" 
-                    className="btn-register">Guardar datos    
-                    </button>
-                </form>
+            <div className="registerBox">
+                <div className="inputBox w50">
+                    <input required type="text" />
+                    <span>NOMBRES</span>
+                </div>
+                <div className="inputBox w50">
+                    <input required type="text" />
+                    <span>APELLIDOS</span>
+                </div>
+                <div className="inputBox w50">
+                    <input required type="text" />
+                    <span>CORREO</span>
+                </div>
+                <div className="inputBox w50">
+                    <input required type="text" />
+                    <span>CONTRASEÑA</span>
+                </div>
+                <div className="inputBox w100">
+                    <input required type="submit" value="Guardar"/>
+                </div>
+            </div>
             </div>
         </div>
 
