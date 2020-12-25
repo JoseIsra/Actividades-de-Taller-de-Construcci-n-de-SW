f40ca8a92e5fdab470ae4474650cce87308fa57a
israel diaz <israelmyname@gmail.com>
2020-12-25 02:28:19 -0500

    formulario registro terminado

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -17,6 +17,7 @@ const validationSchema = yup.object({
 
 export const Register = () => {
     const [message , setMessage] = useState(null);
+    const [visible , setVisible] = useState(false);
     const history = useHistory();
 
     const onSubmit =  async(values, onSubmitProps) => {
@@ -26,15 +27,20 @@ export const Register = () => {
         onSubmitProps.resetForm();
         history.push('/login');
         }else{
-            setTimeout(()=>{
                 setMessage(response.data);
-            },2000);
         }
-        
-        
-        
     }
-    
+    useEffect(()=>{
+        if(!message){
+            setVisible(false);
+            return
+        }
+        setVisible(true);
+        const timer = setTimeout(() => {
+            setVisible(false)
+        }, 2500);
+        return () => clearTimeout(timer);
+    },[message]);
 
 
     return (
@@ -53,7 +59,9 @@ export const Register = () => {
                     </Link>
                     <div className="container">
                         <div className="registerForm">
-                    {message && <p className="emergencia">{message}</p> }
+                    {visible && (<p className="emergencia">
+                    <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-triangle"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"/><line x1="12" y1="9" x2="12" y2="13"/><line x1="12" y1="17" x2="12.01" y2="17"/></svg></span>{message}
+                    </p>)}
                             <h2>Regístrate</h2>
                             <div className="registerBox">
                             <Form>
