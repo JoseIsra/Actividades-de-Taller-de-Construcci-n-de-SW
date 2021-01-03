47deaca2bc2655b6cee3ce75afcd6325c4eb62a6
Marco <marco.fura@unmsm.edu.pe>
2021-01-03 10:56:29 -0500

    Deatail product terminado

--- src/components/DetailProduct/DetailProduct.js
+++ src/components/DetailProduct/DetailProduct.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import { api } from '../../httprequestconfig/methods';
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 import './DetailProduct.css';
 export const DetailProduct = () => {
@@ -21,7 +23,7 @@ export const DetailProduct = () => {
                     idproduct:res.data.idproduct,
                     prod_name:res.data.prod_name,
                     prod_price:res.data.prod_price,
-                    prod_description:res.data.description,
+                    prod_description:res.data.prod_description,
                     prod_image:res.data.prod_image
                 });
             }).catch(err => console.log(err))
@@ -30,9 +32,26 @@ export const DetailProduct = () => {
 
     return (
         <div className="detailProduct">
-            <h1>detalles del producto + imagen + descripcion </h1>
-            {product.prod_name}
-            <img src={product.prod_image} alt="producto cool" />
+            <div className="detailProduct_title">
+                <h1>{product.prod_name}</h1>
+            </div>
+            <div className="detailProduct_price">
+                <h2>Precio: S/. {product.prod_price}</h2>
+            </div>
+            <div className="detailProduct_main">
+                
+                    <button className="detailProduct_button">Agregar</button>
+
+                
+
+            </div>
+            <div className="detailProduct_description">
+                <h2>Descripción: </h2>
+             
+                <p>{product.prod_description}</p>
+            </div>
+            
+            <img className="detailProduct_image" src={product.prod_image} alt="producto cool" />
         </div>
     )
 }
